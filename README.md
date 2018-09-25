abd6968cdf985623daf425456b79945c1b139a02
radu puspana <rpuspana@gmail.com>
2018-09-25 21:55:15 +0300

    Add styles for modal window and add these styles to the Modal component in OptionModal.js Update the readme file with some features of the app

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ in the browser type the address http://localhost:8080/
 
 start the dev server
 Type the command in a terminal making sure you are in the project's root folder: yarn add dev-server
+
+features:
+- great experience for mobile - app uses the device's screen width
+- modular
+- sass, webpack, babel, react-modal, browser style reset
+- responsive
+- cross-browser compatible
--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,14 +7,18 @@ const OptionModal = (props) => (
     // build-in props for Modal from https://github.com/reactjs/react-modal
     // contentLabel = UX for reading web page tools
     // onRequestClose = hit ESC or press outside the modal => close modal 
+    // closeTimeoutMS={Nms} = N ms before taking the Modal div out of the DOM
+    // className="modal" - override the default style of React Modal for modal content, with the modal user-made classs in the file_modal.scss
     <Modal
         isOpen={!!props.selectedOption}
-        contentLabel="Selected option"
         onRequestClose={props.handleClearSelectedOption}
+        contentLabel="Selected option"
+        closeTimeoutMS={250}
+        className="modal"
     >
-        <h3>Selected option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelectedOption}>Okay</button>
+        <h3 className="modal__title">Selected option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="modal__button" onClick={props.handleClearSelectedOption}>Okay</button>
     </Modal>
 );
 
